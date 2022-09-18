99638e4f02d1c885baefe30452098b30fd7dbbf8
Maks Kalinin <shrimp991@mail.ru>
2022-09-18 19:04:33 +0300

    add tests and jacoco report

---
+++ src/test/java/getBurgerPriceTest.java
@@ -0,0 +1,31 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import praktikum.Bun;
+import praktikum.Burger;
+import praktikum.Ingredient;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+public class getBurgerPriceTest {
+    Burger burger;
+    @Mock
+    Bun bun;
+    @Mock
+    Ingredient ingredient;
+
+    @Test
+    public void getBurgerPriceTest(){
+        burger = new Burger();
+        burger.setBuns(bun);
+        burger.addIngredient(ingredient);
+        Mockito.when(bun.getPrice()).thenReturn(50F);
+        Mockito.when(ingredient.getPrice()). thenReturn(100F);
+        float expectedPrice = 200;
+
+        assertEquals("Incorrect value", expectedPrice, burger.getPrice(), 0);
+    }
+}
---
+++ src/test/java/getReceiptTest.java
@@ -0,0 +1,40 @@
+import static org.junit.Assert.assertEquals;
+import static praktikum.IngredientType.SAUCE;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import praktikum.Bun;
+import praktikum.Burger;
+import praktikum.Ingredient;
+
+@RunWith(MockitoJUnitRunner.class)
+public class getReceiptTest {
+  @Mock
+  private Bun bun;
+  @Mock
+  private Ingredient ingredient;
+  private Burger burger;
+
+  @Test
+  public void getReceiptTest() {
+    burger = new Burger();
+    burger.setBuns(bun);
+    burger.addIngredient(ingredient);
+    Mockito.when(bun.getName()).thenReturn("bunNameTest");
+    Mockito.when(ingredient.getType()).thenReturn(SAUCE);
+    Mockito.when(ingredient.getName()).thenReturn("hot sauce");
+    Mockito.when(bun.getPrice()).thenReturn(500F);
+    Mockito.when(ingredient.getPrice()).thenReturn(200F);
+    String actual = burger.getReceipt();
+    String expected = "(==== bunNameTest ====)\r\n"
+        + "= sauce hot sauce =\r\n"
+        + "(==== bunNameTest ====)\r\n"
+        + "\r\n"
+        + "Price: 1200,000000\r\n";
+
+    assertEquals("Incorrect value", expected, actual);
+  }
+}
